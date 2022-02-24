6a6d07cc40a69dc0c8a06c539e1e7732d4a95c93
ozeidan9 <omar.zeidan20@imperial.ac.uk>
2022-02-24 20:06:54 +0000

    setup API

---
+++ API-read.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from urllib.request import urlopen
+import json
+READ_API_KEY = "AALOIQGCNR869UZ5"
+CHANNEL_ID = "1662430"
+
+def main():
+    conn = urlopen("http://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" \
+                           % (CHANNEL_ID,READ_API_KEY))
+
+    response = conn.read()
+    http_status_code = conn.getcode()
+
+    if (http_status_code == 200):
+        print("HTTP Connection Successfull!")
+    else:
+        print("HTTP Connection Failed!")
+
+    data=json.loads(response)
+    print (data['field1'])
+    conn.close()
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ API-write.py
@@ -0,0 +1,18 @@
+import urllib.request
+from urllib.request import urlopen
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context # uses unverified to bypass certificate error
+# import requests
+
+msg = "Correct"
+msg = msg.replace('\n', "%0A")
+
+b=urlopen('https://api.thingspeak.com/update?api_key=LKDF3RROONZUUTAS&field2='+msg)
+print("\nYour message has successfully been sent!")
+
+
+
+
+# msg=requests.get("https://thingspeak.com/channels/1461170/field/1")
+# msg=msg.json()['feeds'][-1]['field1']
+# print("\nThe Message sent was: \n\n"+str(msg))
