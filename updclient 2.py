b59e4b0e15ee7bf7927ebb984ff7fdcc06eb0ba4
ozeidan9 <omar.zeidan20@imperial.ac.uk>
2022-03-01 21:01:33 +0000

    push

--- updclient 2.py
+++ updclient 2.py
@@ -8,7 +8,7 @@ server_port = 12000
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #Bind UDP client 2 to port 13000
-client_socket.bind((server_name, 13000))
+client_socket.bind(('localhost', 13000))
 
 #some work
 msg = input("Enter message into client 2: ");
