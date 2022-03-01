a7f8d0ffad7680b2dab90f55244ce178d57634b9
ozeidan9 <omar.zeidan20@imperial.ac.uk>
2022-03-01 21:50:18 +0000

    updastes

--- updclient 2.py
+++ updclient 2.py
@@ -2,19 +2,19 @@ import socket
 print("We're in udp client...");
 
 #the server name and port client wishes to access
-server_name = 'localhost'
+local_host_name = 'localhost'
 server_port = 12000
 #create a UDP client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #Bind UDP client 2 to port 13000
-client_socket.bind(('localhost', 13000))
+client_socket.bind(('localhost', 13000))  # only works for localhost
 
-#some work
+#some workhello
 msg = input("Enter message into client 2: ");
 
 #send the message  to the udp server
-client_socket.sendto(msg.encode(),(server_name, server_port))
+client_socket.sendto(msg.encode(),(local_host_name, server_port))
 
 
 client_socket.close()
