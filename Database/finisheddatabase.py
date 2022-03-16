ca3ce96d25e80fa0a97b0fd958f98b79d7970ac5
15ajones <15ajones0201@gmail.com>
2022-03-16 19:14:28 +0000

    cool cool

--- Database/finisheddatabase.py
+++ Database/finisheddatabase.py
@@ -122,6 +122,8 @@ def main():
     players = []
     UI_cadd = "none"
     game_type = ""
+    memory_pattern = ""
+    memory_players = []
     
     while True:
         if not game_started:
@@ -136,20 +138,39 @@ def main():
             cmsg = cmsg.decode()
             cmsg = cmsg.split()
             if len(cmsg)==2 and cmsg[0]=="admin" and cmsg[1]=="start":
-                game_started = True
-                cmsg = "game started"
-                print(cmsg)
-                server_socket.sendto(cmsg.encode(), cadd)
-                wordle = ""
-                if UI_cadd != "none":
-                    cmsg = "select"
-                    server_socket.sendto(cmsg.encode(), UI_cadd)
-                for i in range(5):
-                    x = int(randrange(4))
-                    wordle+=wordle_characters[x]
-                print(wordle)
+                if cmsg[2] == "mastermind":
+                    game_type = "mastermind"
+                    game_started = True
+                    cmsg = "game mastermind started"
+                    print(cmsg)
+                    server_socket.sendto(cmsg.encode(), cadd)
+                    wordle = ""
+                    if UI_cadd != "none":
+                        cmsg = "select"
+                        server_socket.sendto(cmsg.encode(), UI_cadd)
+                    for i in range(5):
+                        x = int(randrange(4))
+                        wordle+=wordle_characters[x]
+                    print(wordle)
+                elif cmsg[2] == "memory":
+                    game_type = "memory"
+                    game_started = True
+                    cmsg = "game memory started"
+                    print(cmsg)
+                    server_socket.sendto(cmsg.encode(), cadd)
+                    wordle = ""
+                    if UI_cadd != "none":
+                        cmsg = "right"
+                        server_socket.sendto(cmsg.encode(), UI_cadd)
+                        cmsg = "select"
+                        server_socket.sendto(cmsg.encode(), UI_cadd)
+                    memory_pattern = ""
+                    memory_players = players
+                    players_left = len(memory_players)
+      
             elif cmsg[0]=="ui" or cmsg[0]=="UI":
                 UI_cadd = cadd
+
             else:
                 is_admin = True if number_players == 0 else False
                 players.append(player_to_add)
@@ -168,61 +189,113 @@ def main():
             # client then sends ggrgy for example to all clients (g means right character, right place, y means right character, wrong place, r means wrong character)
             # When game ends, the server sends all clients the message: "Game over! x wins"
             while game_started:
-                for player in players: #go through each player's turn
-                    if not game_started:#if game has ended we can end the for loop and leave this section
-                        break
-                    else:
-                        cmsg = "Your turn"
+                if game_type == "mastermind":
+                    for player in players: #go through each player's turn
+                        if not game_started:#if game has ended we can end the for loop and leave this section
+                            break
+                        else:
+                            if UI_cadd != "none":
+                                cmsg = "turn " +  player[0] 
+                                server_socket.sendto(cmsg.encode(), UI_cadd)
+                            cmsg = "Your turn"
+                            # print(cmsg)
+                            # print(player[0])
+                            # print(player[1])
+                            server_socket.sendto(cmsg.encode(), (player[0], int(player[1])))
+                            valid_reply = False
+                            while not valid_reply: #keep searching for replies until we get a 5 bit word from the player who's go it is.
+                                cmsg, cadd = server_socket.recvfrom(2048)
+                                guess = cmsg.decode()
+                                print(guess)
+                                # if len(guess) == 5: #need to add code to make sure its the current player and the guess length is 5)
+                                valid_reply = True
+                            guess_reply = ""
+                            correct_guess = True
+                            for i in range(5): # in this loop we derive the wordle reply to a guess
+                                if guess[i]==wordle[i]:
+                                    guess_reply += "g"
+                                elif guess[i] in wordle:
+                                    guess_reply += "y"
+                                    correct_guess = False
+                                else:
+                                    guess_reply += "r"
+                                    correct_guess = False
+                            if UI_cadd != "none":
+                                cmsg = "outcome " +  guess_reply
+                                server_socket.sendto(cmsg.encode(), UI_cadd)
+                            if correct_guess: # if correct guess we send a message saying game over to everyone, and end this section
+                                if UI_cadd != "none":
+                                    cmsg = "game over winner " +  player[0] 
+                                    server_socket.sendto(cmsg.encode(), UI_cadd)
+                                game_started = False
+                                cmsg = "Correct guess! Game over! Winner is: " + str(player[1])
+                                for x in players:
+                                    server_socket.sendto(cmsg.encode(), (x[0], int(x[1])))
+                                time.sleep(5)
+                                if UI_cadd != "none":
+                                    cmsg = "menu"
+                                    server_socket.sendto(cmsg.encode(), UI_cadd)
+                
+                elif game_type == "memory":
+                    if player == 1:
+                        game_started = False
+                        for player in players:
+                            if player!=0:
+                                winner = player
+                                break
+                        if UI_cadd != "none":
+                            cmsg = "game over winner " +  player[0] 
+                            server_socket.sendto(cmsg.encode(), UI_cadd)
+                            time.sleep(5)
+                            cmsg = "menu " +  player[0] 
+                            server_socket.sendto(cmsg.encode(), UI_cadd)
+
+                    i = -1
+                    for player in memory_players:
+                        if not game_started:#if game has ended we can end the for loop and leave this section
+                            break
+                        i+=1
+                        if player == 0:
+                            continue
                         if UI_cadd != "none":
                             cmsg = "turn " +  player[0] 
                             server_socket.sendto(cmsg.encode(), UI_cadd)
-                        print(cmsg)
-                        print(player[0])
-                        print(player[1])
+                        cmsg = "Your turn"
                         server_socket.sendto(cmsg.encode(), (player[0], int(player[1])))
-                        valid_reply = False
-                        while not valid_reply: #keep searching for replies until we get a 5 bit word from the player who's go it is.
-                            cmsg, cadd = server_socket.recvfrom(2048)
-                            guess = cmsg.decode()
-                            print(guess)
-                            # if len(guess) == 5: #need to add code to make sure its the current player and the guess length is 5)
-                            valid_reply = True
-                        guess_reply = ""
-                        correct_guess = True
-                        for i in range(5): # in this loop we derive the wordle reply to a guess
-                            if guess[i]==wordle[i]:
-                                guess_reply += "g"
-                            elif guess[i] in wordle:
-                                guess_reply += "y"
-                                correct_guess = False
-                            else:
-                                guess_reply += "r"
-                                correct_guess = False
-                        if UI_cadd != "none":
-                            cmsg = "outcome " +  guess_reply
-                            server_socket.sendto(cmsg.encode(), UI_cadd)
-                        if correct_guess: # if correct guess we send a message saying game over to everyone, and end this section
-                            if UI_cadd != "none":
-                                cmsg = "game over winner " +  player[0] 
-                                server_socket.sendto(cmsg.encode(), UI_cadd)
-                            game_started = False
-                            cmsg = "Correct guess! Game over! Winner is: " + str(player[1])
-                            for x in players:
-                                server_socket.sendto(cmsg.encode(), (x[0], int(x[1])))
-                            time.sleep(5)
-                            if UI_cadd != "none":
-                                cmsg = "menu"
-                                server_socket.sendto(cmsg.encode(), UI_cadd)
+                        cmsg, cadd = server_socket.recvfrom(2048)
+                        cmsg = cmsg.decode()
+                        if memory_pattern == "": #start of new pattern
+                            memory_pattern += cmsg[0]
+                        else: 
+                            if cmsg[0:len(memory_pattern)] == memory_pattern and len(cmsg) == len(memory_pattern)+1: # player got pattern right, extends it
+                                memory_pattern = cmsg
+                                if UI_cadd != "none":
+                                    cmsg = "move " + cmsg[-1]
+                                    server_socket.sendto(cmsg.encode(), UI_cadd)
+                            else: #player got pattern wrong, is eliminated.
+                                memory_players[i] = 0;
+                                players_left-=1
+                                if players_left == 1:
+                                    game_started = False
+                                    break
+                                memory_pattern = ""
 
-                        # else:
-                        #     for x in players:
-                        #         server_socket.sendto(guess_reply.encode(), (x[0], int(x[1])))
+
+
+
+                        
+#ASK IF ADMIN/ BOARDS NEED TO BE TOLD WHEN THE GAME ENDS
+
                             
 
 
                         
 
-                            
+#turn user1
+#move L
+#eliminated user1
+#winner user2  
+                     
 #to do: update points after a winner - do it when working with Omar
 
 if __name__ == "__main__":
