325d81512b1a05f464f7728b89a2414db7e4f389
ozeidan9 <omar.zeidan20@imperial.ac.uk>
2022-03-20 23:57:45 +0000

    y

--- Clients/combined_board1_client.py
+++ Clients/combined_board1_client.py
@@ -12,7 +12,7 @@ import os.path
 def main():
 
     #the server name and port client wishes to access
-    board_server_name = '192.168.137.70'  #ip of arduino (subject to change - CHANGES
+    board_server_name = '192.168.137.213'  #ip of arduino (subject to change - CHANGES
     board_server_port = 11000
     #create a TCP client socket
     board_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
