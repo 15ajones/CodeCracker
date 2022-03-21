e0243ba58475bf36815c9c9dd276ecdfb3435b21
15ajones <15ajones0201@gmail.com>
2022-03-21 12:27:59 +0000

    ok

--- Database/database.py
+++ Database/database.py
@@ -1,103 +1,59 @@
-# import boto3
+import boto3
 import socket
 from decimal import Decimal
 from pprint import pprint
-# from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Key
 from random import randrange
 import time
-# def create_user_table(dynamodb=None):
-#     if not dynamodb:
-#         dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
 
-#     table = dynamodb.create_table(
-#         TableName='Users',
-#         KeySchema=[
-#             {
-#                 'AttributeName': 'User', # the User value is the socket value of a player
-#                 'KeyType': 'HASH'  # Partition key
-#             },
-#             {
-#                 'AttributeName': 'Name', #the user name the user wants to send
-#                 'KeyType': 'RANGE' # Sort key
-#             } 
-#         ],
-#         AttributeDefinitions=[
-#             {
-#                 'AttributeName': 'User',
-#                 'AttributeType': 'S'
-#             },
-#             {
-#                 'AttributeName': 'Name',
-#                 'AttributeType': 'S'
-#             }
-#         ],
-#         ProvisionedThroughput={
-#             'ReadCapacityUnits': 10,
-#             'WriteCapacityUnits': 10
-#         }
-#     )
-#     print("created table")
-#     return table
+def add_user(user,name,is_admin,dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('Users')
+    response = table.put_item(
+        Item={
+            'User': user,
+            'Name' : name,
+            'info': {
+                'points': 0,
+                'is_admin' : is_admin
+            }
+        }
+    )
+    print("added user")
+    return response
 
-# def add_user(user,name,is_admin,dynamodb=None):
-#     if not dynamodb:
-#         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-#     table = dynamodb.Table('Users')
-#     response = table.put_item(
-#         Item={
-#             'User': user,
-#             'Name' : name,
-#             'info': {
-#                 'points': 0,
-#                 'is_admin' : is_admin
-#             }
-#         }
-#     )
-#     print("added user")
-#     return response
+def update_points(user, name, point):#pass param is the new password
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('Users')
+    response = table.update_item(
+        Key={
+            'User':user,
+            'Name':name
+        },
+        UpdateExpression = "set info.points=:r",
+        ExpressionAttributeValues={
+            ':r': point
+        },
+        ReturnValues = "UPDATED_NEW"
+    )
+    return response
 
-# def update_points(user, name, point):#pass param is the new password
-#     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-#     table = dynamodb.Table('Users')
-#     response = table.update_item(
-#         Key={
-#             'User':user,
-#             'Name':name
-#         },
-#         UpdateExpression = "set info.points=:r",
-#         ExpressionAttributeValues={
-#             ':r': point
-#         },
-#         ReturnValues = "UPDATED_NEW"
-#     )
-#     return response
+def query_user(user, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
+    table = dynamodb.Table('Users')
+    response = table.query(
+        KeyConditionExpression=Key('User').eq(user)
+    )
+    results = response['Items']
+    for result in results:
+        return(result)
+    print("finished query")
 
-# def query_user(user, dynamodb=None):
-#     if not dynamodb:
-#         dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
-#     table = dynamodb.Table('Users')
-#     response = table.query(
-#         KeyConditionExpression=Key('User').eq(user)
-#     )
-#     results = response['Items']
-#     for result in results:
-#         return(result)
-#     print("finished query")
-
-# def delete_user_table(dynamodb=None):
-#     try:
-#         if not dynamodb:
-#             dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
-#         table = dynamodb.Table('Users')
-#         print("deleted table")
-#         table.delete()
-#     except:
-#         pass
-
+#{'info': {'is_admin': True, 'points': Decimal('0')}, 'User': '62125', 'Name': 'board1'}
 def main():
-#     print("Server has started running")
-#     # delete_user_table()
-#     table = create_user_table()
+
     server_port = 12000
     #create a UDP socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -109,12 +65,6 @@ def main():
     #Now the loop that actually listens from clients
     #The same server socket serves all clients here
 
-    # board1 = ('0.0.0.0', 11000)
-    # board2 = ('0.0.0.0', 12000)
-    # board3 = ('0.0.0.0', 13000)
-    # board4 = ('0.0.0.0', 14000)
-    # board5 = ('0.0.0.0', 15000)
-    # board6 = ('0.0.0.0', 16000)
     wordle_characters = ["L","R","1","2","3"]
     wordle_length = 5
     game_started = False
@@ -126,14 +76,9 @@ def main():
     memory_players = []
     menu_options = ["mastermind", "memory", "leaderboard"]
     current_menu_select = 0
-    
+    msgs_to_receive = 0
     while True:
         if not game_started:
-            # two types of messages from client:
-            # 1) new user joins - just sends his name
-            #     example message: user1
-            # 2) admin starts game
-            #     message: admin start
             
             cmsg, cadd = server_socket.recvfrom(2048)
             player_to_add = [str(cadd[0]), int(cadd[1])]
@@ -141,8 +86,12 @@ def main():
             cmsg = cmsg.decode()
             print(cmsg)
             cmsg = cmsg.split()
-
-            if len(cmsg)==2 and cmsg[0]=="admin":
+            if msgs_to_receive > 0:
+                msgs_to_receive = 0
+                print("ignored message")
+                cmsg = "game over"
+                server_socket.sendto(cmsg.encode(), (cadd[0], int(cadd[1])))
+            elif len(cmsg)==2 and cmsg[0]=="admin":
                 if cmsg[1] == "L":
                     if UI_cadd != "none" and current_menu_select > 0:
                         cmsg = "left"
@@ -192,13 +141,13 @@ def main():
                         if UI_cadd != "none":
                             cmsg = "select"
                             server_socket.sendto(cmsg.encode(), UI_cadd)
-                        memory_pattern = ""
-                        memory_players = players
-                        players_left = len(memory_players)
+                        for i in range(5):
+                            x = int(randrange(4))
+                            wordle+=wordle_characters[x]
+                        print(wordle)
                         cmsg = "memory"
                         for player in players:
                             server_socket.sendto(cmsg.encode(), (player[0], int(player[1])))
-                        i = -1
 
             elif cmsg[0]=="ui" or cmsg[0]=="UI":
                 UI_cadd = cadd
@@ -206,8 +155,15 @@ def main():
             else:
                 is_admin = True if number_players == 0 else False
                 players.append(player_to_add)
-                # add_user(str(cadd), str(cmsg[0]), is_admin)
+                add_user(str(player_to_add[1]), str(cmsg[0]), is_admin)
+                print(query_user(str(player_to_add[1])))
                 number_players+=1
+                if UI_cadd != "none":
+                    cmsg = "leaderboard " + str(cmsg[0]) + " 0"
+                    print("sending", cmsg)
+                    server_socket.sendto(cmsg.encode(), UI_cadd)
+                    print("sent")
+                    time.sleep(4)
                 if is_admin:
                     cmsg = "admin"
                 else:
@@ -226,8 +182,11 @@ def main():
                             break
                         else:
                             if UI_cadd != "none":
-                                cmsg = "turn " +  player[0] 
+                                # cmsg = "turn " +  player[0] 
+                                cmsg = "turn " + str(query_user(str(player[1]))['Name'])
+                                print("sending", cmsg)
                                 server_socket.sendto(cmsg.encode(), UI_cadd)
+                                print("sent")
                                 time.sleep(4)
                             cmsg = "your turn"
                             # print(cmsg)
@@ -259,8 +218,12 @@ def main():
                                 print("sent outcome")
                                 time.sleep(5)
                             if correct_guess: # if correct guess we send a message saying game over to everyone, and end this section
+                                point = int(query_user(str(player[1]))['info']['points'])
+                                name = str(query_user(str(player[1]))['Name'])
+                                point+=1
+                                update_points(str(player[1]),name,point)
                                 if UI_cadd != "none":
-                                    cmsg = "winner " +  str(player[0]) 
+                                    cmsg = "winner " +  str(query_user(str(player[1]))['Name'])
                                     server_socket.sendto(cmsg.encode(), UI_cadd)
                                     time.sleep(5)
                                 game_started = False
@@ -271,69 +234,91 @@ def main():
                                 if UI_cadd != "none":
                                     cmsg = "menu"
                                     server_socket.sendto(cmsg.encode(), UI_cadd)
-                                    cmsg = "leaderboard omar 0"
+                                    cmsg = "leaderboard"
+                                    for n in players:
+                                        cmsg+=" " +str(query_user(str(n[1]))['Name'])+ " "+ str(int(query_user(str(n[1]))['info']['points']))
                                     server_socket.sendto(cmsg.encode(), UI_cadd)
                                     time.sleep(5)
                                 
                 
                 elif game_type == "memory":
-                    if players_left == 1:
+                    msgs_to_receive = len(players)
+                    t = int(randrange(15))
+                    time.sleep(t)
+                    if UI_cadd != "none":
+                        cmsg = "start " + wordle
+                        print("sending", cmsg)
+                        server_socket.sendto(cmsg.encode(), UI_cadd)
+                        print("sent")
+                        cmsg = "your turn"
+                        for player in players:
+                            cmsg = "your turn"
+                            server_socket.sendto(cmsg.encode(), (player[0], int(player[1])))
+                            print("sent to client")
+
+                    replies = 0
+                    while replies<2:
+                        cmsg, cadd = server_socket.recvfrom(2048)
+                        guess = cmsg.decode()
+                        msgs_to_receive = msgs_to_receive - 1
+                        print("guess is: ", guess)
+                        if guess == wordle:
+                            game_started = False
+                            game_has_ended = True
+                            # for player in players:
+                            #     cmsg = "game over"
+                            #     server_socket.sendto(cmsg.encode(), (player[0], int(player[1])))
+                            cmsg = "game over"
+                            server_socket.sendto(cmsg.encode(), (cadd[0], int(cadd[1])))
+                            point = int(query_user(str(cadd[1]))['info']['points'])
+                            name = str(query_user(str(cadd[1]))['Name'])
+                            point+=1
+                            update_points(str(cadd[1]),name,point)
+                            if UI_cadd:
+                                cmsg = "winner " + str(query_user(str(cadd[1]))['Name'])
+                                server_socket.sendto(cmsg.encode(), UI_cadd)
+                                time.sleep(2)
+                                cmsg = "menu"
+                                server_socket.sendto(cmsg.encode(), UI_cadd)
+                                time.sleep(1)
+                                for n in players:
+                                    cmsg = "leaderboard " + str(query_user(str(n[1]))['Name'])+ " "+ str(int(query_user(str(n[1]))['info']['points']))
+                                    server_socket.sendto(cmsg.encode(), UI_cadd)
+                                    time.sleep(1)
+                            break
+                        else:
+                            cmsg = "game over"
+                            server_socket.sendto(cmsg.encode(), (cadd[0], int(cadd[1])))
+                            replies+=1
+
+
+                    if replies>1:
                         game_started = False
-                        for player in memory_players:
-                            if player!=0:
-                                winner = player
-                                break
-                        cmsg = "game over"
-                        for x in players:
-                            server_socket.sendto(cmsg.encode(), (x[0], int(x[1])))
-                        if UI_cadd != "none":
-                            cmsg = "winner " +  str(winner[0])#change later
-                            server_socket.sendto(cmsg.encode(), UI_cadd)
-                            time.sleep(4)
-                            cmsg = "menu"
+                        for player in players:
+                            cmsg = "game over"
+                            server_socket.sendto(cmsg.encode(), (player[0], int(player[1])))
+                        if UI_cadd:
                             server_socket.sendto(cmsg.encode(), UI_cadd)
                             time.sleep(2)
-                            cmsg = "leaderboard omar 0"
+                            cmsg = "menu"
                             server_socket.sendto(cmsg.encode(), UI_cadd)
+                            time.sleep(1)
+                            for n in players:
+                                    cmsg = "leaderboard " + str(query_user(str(n[1]))['Name'])+ " "+ str(int(query_user(str(n[1]))['info']['points']))
+                                    server_socket.sendto(cmsg.encode(), UI_cadd)
+                                    time.sleep(1)
+                            break
+
+
+
+
+
+
 
                     
-                    for player in memory_players:
-                        if UI_cadd != "none":
-                            cmsg = "players"
-                            for x in memory_players:
-                                cmsg+=" "
-                                cmsg+=str(x[0])
-                            server_socket.sendto(cmsg.encode(), UI_cadd)
-                            time.sleep(4)
-                        if not game_started:#if game has ended we can end the for loop and leave this section
-                            break
-                        i+=1
-                        if player == 0:
-                            continue
-                        if UI_cadd != "none":
-                            cmsg = "turn " +  player[0] 
-                            server_socket.sendto(cmsg.encode(), UI_cadd)
-                            time.sleep(4)
-                        cmsg = "your turn"
-                        server_socket.sendto(cmsg.encode(), (player[0], int(player[1])))
-                        cmsg, cadd = server_socket.recvfrom(2048)
-                        cmsg = cmsg.decode()
-                        if memory_pattern == "": #start of new pattern
-                            memory_pattern += cmsg[0]
-                        else: 
-                            if cmsg[0:len(memory_pattern)] == memory_pattern and len(cmsg) == len(memory_pattern)+1: # player got pattern right, extends it
-                                memory_pattern = cmsg
-                                if UI_cadd != "none":
-                                    cmsg = "move " + cmsg[-1]
-                                    server_socket.sendto(cmsg.encode(), UI_cadd)
-                                    time.sleep(4)
-                            else: #player got pattern wrong, is eliminated.
-                                memory_players[i] = 0;
-                                players_left-=1
-                                if players_left == 1:
-                                    game_started = False
-                                    break
-                                memory_pattern = ""
+                    
+
+                        
 
 
 
